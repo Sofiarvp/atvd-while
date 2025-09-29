b1a510b79e49c0391c1a7bd59ac5003f0626e514
Sofiarvp <sofiarvp77@gmail.com>
2025-09-29 18:22:25 -0300

    Add files via upload

---
+++ atvd_while.py
@@ -0,0 +1,94 @@
+
+    
+''' comente bloco por bloco
+1
+i=1
+while i<=10:
+    print(i)
+    i+=1
+'''
+'''
+2   
+i=10
+while i>=1:
+    print (i)
+    i-=1
+'''    
+    
+'''
+3
+i=0
+while i<=20:
+    print(i)
+    i+=2
+
+    
+'''
+
+'''
+4
+i=1
+while i<=20 and i>1:
+    print(i)
+    i+=2
+
+    
+'''
+'''
+5
+i=1
+numero=int(input('digite um numero:'))
+while i<=numero:
+    print (i)
+    i+=1
+'''
+    
+'''
+6
+i=0
+palavra=input('digite uma palavra:')
+while i<5:
+    print (palavra)
+    i+=1
+   '''     
+        
+'''
+7
+i=0
+while i<=10:
+    print(f"2 x {i} = {2*i}")
+    i+=1
+
+'''  
+'''
+8
+i=0
+numero=float(input('digite um numero:'))
+while i<10:
+    print(f"0 x {i} = {0*i}")
+    print(f"1 x {i} = {1*i}")
+    print(f"2 x {i} = {2*i}")
+    print(f"3 x {i} = {3*i}")
+    print(f"4 x {i} = {4*i}")
+    print(f"5 x {i} = {5*i}")
+    print(f"6 x {i} = {6*i}")
+    print(f"7 x {i} = {7*i}")
+    
+    
+    i+=i
+'''
+
+9
+i=0
+nome1=(input('digite um nome:'))
+nome2=(input('digite um nome:'))
+nome3=(input('digite um nome:'))
+nome4=(input('digite um nome:'))
+nome5=(input('digite um nome:'))
+while i<2:
+    lista=[nome1, nome2, nome3, nome4, nome5]
+    print (lista)
+
+
+
+
